95087a89da4de3d007f9d362f4b8ba86e2214b08
jecihjoy <jecihjoy@gmail.com>
2020-02-09 21:56:50 +0300

    Initial project setup

---
+++ src/main/java/com/example/SpringCoreExample/SpringCoreExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringCoreExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringCoreExample/SpringCoreExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringCoreExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
