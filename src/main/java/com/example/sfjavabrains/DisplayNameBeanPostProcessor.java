e4f49370441852cf8b3eed14d3a38876fdcae7e1
jacinta254 <jecihjoy@gmail.com>
2022-02-22 15:58:23 +0300

    Spring extension points; BeanPostProcessor & BeanFactoryPostProcessor

---
+++ src/main/java/com/example/sfjavabrains/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,18 @@
+package com.example.sfjavabrains;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DisplayNameBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Before Initialization method " + beanName);
+        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("After Initialization method " + beanName);
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+}
